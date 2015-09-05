4c12fd7863a06154eb66e3fbc8cb70e008d226b6
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-09-04 21:31:27 -0400

    Initial Commit

---
+++ HappyLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyLearn
+//
+//  Created by Yifan Xiao on 9/4/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
