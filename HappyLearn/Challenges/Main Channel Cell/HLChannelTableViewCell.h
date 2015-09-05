364016e582f3538c12948600d06b10c05aaa0008
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-09-05 13:18:10 -0400

    Update of cells.

---
+++ HappyLearn/Challenges/Main Channel Cell/HLChannelTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  HLChannelTableViewCell.h
+//  HappyLearn
+//
+//  Created by Yifan Xiao on 9/5/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLChannelTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *challengeCountLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *channelIconImageView;
+
+
+@end
---
+++ HappyLearn/Challenges/Main Channel Cell/HLChannelTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  HLChannelTableViewCell.m
+//  HappyLearn
+//
+//  Created by Yifan Xiao on 9/5/15.
+//  Copyright (c) 2015 Yifan Xiao. All rights reserved.
+//
+
+#import "HLChannelTableViewCell.h"
+
+@implementation HLChannelTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
